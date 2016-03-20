2bfbf0038d306d6dd21b8c7655bd0798b36291e8
michealmina2 <michealmina2@gmail.com>
2016-03-21 00:55:14 +0200

    some contribution to FCFS

--- hellowx.py
+++ hellowx.py
@@ -1,85 +1,76 @@
 import wx
+import sys
+def Wiki_Question_Handler(Answer):
+	if Answer == wx.ID_NO:
+		AA=wx.MessageDialog(None,"Please click help from the menu bar then choose Wiki\nTo understand how to use the program",'Wiki',wx.OK)
+		A=AA.ShowModal()
+		AA.Destroy()
+
+
+
 
 class bucky(wx.Frame):
-	
+
+	total_time = 0
+	average_waiting=0
+
 	def __init__(self,parent,id):
-		#Main things
-		wx.Frame.__init__(self,parent,id,'Anything for now',size=(1000,500))
+
+
+		#Did you read the wiki?
+		WikiQuestion=wx.MessageDialog(None,'Did you read the "how to use" Wiki?','Title',wx.YES_NO)
+		WikiQuestion_Answer = WikiQuestion.ShowModal()
+		WikiQuestion.Destroy()
+		Wiki_Question_Handler(WikiQuestion_Answer)
+
+		#Single Choice Dialogue (Choose Scheduler mode)
+		select=wx.SingleChoiceDialog(None,'Please choose the type of your desired scheduler','Scheduler Choice',['FCFS','SJF','Priority','Round Robin'])
+		if select.ShowModal()== wx.ID_CANCEL:
+			sys.exit()
+		Scheduler_Mode_Answer=select.GetStringSelection()
+		select.Destroy()
+
+
+
+		#Main Frame
+		wx.Frame.__init__(self,parent,id,Scheduler_Mode_Answer+" Scheduler",size=(1000,500))
 		panel=wx.Panel(self)
 
-		#Buttons
-		button=wx.Button(panel,label="exit",pos=(130,10),size=(60,60))
-		button.SetForegroundColour('green')
-		button.SetBackgroundColour('blue')
-		self.Bind(wx.EVT_BUTTON,self.closebutton, button)
-		self.Bind(wx.EVT_CLOSE, self.closewindow)
+		#Arrival Time things
+		Arrival_Time_Static=wx.StaticText(panel,-1,'Arrival time',pos=(10,10),size=(-1,-1),style=0)
+		Arrival_Time_Text=wx.TextCtrl(panel,pos=(75,8),size=(100,20))
+		Arrival_Time_Text.Value='0'
 
-		#Menubar and statusbar
-		status=self.CreateStatusBar()
-		menubar=wx.MenuBar()
-		first=wx.Menu()
-		second=wx.Menu()
-		first.Append(wx.NewId(),"New ndow","This is a new window")
-		first.Append(wx.NewId(),"Open...","Tndow")
-		menubar.Append(first,"File")
-		menubar.Append(second,"Edit")
-		self.SetMenuBar(menubar)
+		#Burst Time things
+		Burst_Time_Static=wx.StaticText(panel,-1,'Burst time',pos=(10,30),size=(-1,-1),style=0)
+		Burst_Time_Text=wx.TextCtrl(panel,pos=(75,28),size=(100,20))
+		Burst_Time_Text.Value='0'
 
+		#Add process button
+		Add_Process=wx.Button(panel,label="Add process",pos=(200,15),size=(-1,-1))
+		Add_Process.SetForegroundColour('red')
+		Add_Process.SetBackgroundColour('yellow')
+		self.Bind(wx.EVT_BUTTON,lambda event: self.Add_Process_EVT(event,Burst_Time_Text,Arrival_Time_Text), Add_Process)
 
-		#Message Dialog yes/no question
-		box=wx.MessageDialog(None,'Did you read the how to use Wiki?','Title',wx.YES_NO)
-		answer = box.ShowModal()
-		box.Destroy()
+		#Finish process addition button
+		Finish=wx.Button(panel,label="Done",pos=(300,15),size=(-1,-1))
+		self.Bind(wx.EVT_BUTTON,self.Finish_EVT, Finish)
 
-		#Text box to take entry
-		textbox=wx.TextEntryDialog(None, "Please enter something", "Title", 'Click to enter')
-		if textbox.ShowModal()==wx.ID_OK:
-			answer2=textbox.GetValue()
-
-		#Single Choice Dialogue
-		select=wx.SingleChoiceDialog(None,'Which mode?','Mode Choice',['LIFO','FIFO','What so ever'])
-		
-		
-		if select.ShowModal()==wx.ID_OK:
-			answer3 = select.GetStringSelection()
-
-		#Image Button
-		#pic=wx.Image("1.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
-		#self.button123=wx.BitmapButton(panel,-1, pic, pos=(150,150))
-		#self.Bind(wx.EVT_BUTTON, self.closebutton, self.button123)
-
-		#Slider
-		slider = wx.Slider(panel,-1 , 50, 1, 100, pos=(300,0), size=(250,-1), style=wx.SL_AUTOTICKS | wx.SL_LABELS)
-		slider.SetTickFreq(20, 1)
-
-		#Spinner
-		spinner= wx.SpinCtrl(panel, -1, "", (500,250), (100,-1))
-		spinner.SetRange(1,100)
-		spinner.SetValue(100)
-
-		#Checkbox
-		CHKBX=wx.CheckBox(panel, -1, "TEST",(50,200),(100,-1))
-
-		#Test in static text
-		tester=(str)(slider.GetValue())
-		statictextbox=wx.StaticText(panel,-1, tester, (600,0))
+	def Add_Process_EVT(self,event,Burst,Arrival):
+		self.total_time=self.total_time+(int)(Burst.GetValue())
+		Burst.Value='0'
+		Arrival.Value='0'
 
 
-		#List boxes
-		MyList=['Hello','List','Boxes']
-		container = wx.ListBox(panel, -1, (800,50), (80,-1), MyList, wx.LB_SINGLE)
-		container.SetSelection(2)
+	def Finish_EVT(self,event):
+		Tester=wx.MessageDialog(None,"Total times=" + (str)(self.total_time),'Title',wx.OK)
+		Tester1 = Tester.ShowModal()
+		if Tester1==wx.ID_OK:
+			sys.exit()
 
-	def closebutton(self,event):
-		self.Close(True)
-		
-	def closewindow(self,event):
-		self.Destroy()
-		
 if __name__=='__main__' :
 	app=wx.PySimpleApp()
 	frame=bucky(parent=None,id=-1)
 	frame.Show()
 	app.MainLoop()
-	
 	
