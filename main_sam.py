8012e5bb930284f0cff3ff43e1378f3b28479084
SamuelWilson10 <samuel_elo@hotmail.com>
2016-03-26 07:23:23 +0200

    Updated Scheduler.py

--- main_sam.py
+++ main_sam.py
@@ -2,22 +2,22 @@ import schedulers
 import datetime
 if __name__=='__main__' :
     p_fcfs = [
-        (1, 20, 2), (2, 0, 3), (3, 5, 10)
+        (1, 7, 2), (2, 13, 2), (3, 3, 2), (4, 9, 2)
     ]
     schedulers.fcfs(p_fcfs)
-
+    schedulers.reset()
     schedulers.draw_svg(["FCFS",
                          [("Sam", 5, 5),
                           ("Mina", 10, 5),
                           ]])
 
     p_sjf_non = [
-        (1, 2, 2), (2, 0, 1), (3, 1, 4), (4, 0, 2)
+        (1, 2, 2), (2, 5, 1), (3, 1, 4), (4, 5, 2)
     ]
-    # schedulers.reset()
-    # schedulers.sjf_non_preemptive(p_sjf_non)
-    # schedulers.reset()
+    schedulers.sjf_non_preemptive(p_sjf_non)
+    schedulers.reset()
     p_priority_non = [
         (1, 2, 2, 1), (2, 0, 1, 3), (3, 1, 4, 4), (4, 0, 2, 2)
     ]
-    # schedulers.priority_non_preemptive(p_priority_non)
+    schedulers.priority_non_preemptive(p_priority_non)
+    schedulers.reset()
--- schedulers.py
+++ schedulers.py
@@ -15,8 +15,8 @@ def draw_svg(info):
 
     for task in info[1]:
         t = gantt.Task(name=task[0],
-                start=datetime.datetime.fromtimestamp(time_now + task[1]),
-                duration=task[2])
+                       start=datetime.datetime.fromtimestamp(time_now + task[1]),
+                       duration=task[2])
         p1.add_task(t)
 
     p1.make_svg_for_tasks(filename='test_p1.svg', today=datetime.datetime.now())
@@ -38,18 +38,46 @@ def get_avg(values):
 proc_table = []
 avg_waiting = []
 avg_turnaround = []
-final_ans = []
 gantt_chart = OrderedDict()
 time = 0
 
 
 def reset():
+    """
+    :return: Void
+    """
     global proc_table, avg_waiting, avg_turnaround, time
     proc_table = []
     avg_waiting = []
     avg_turnaround = []
 
     time = 0
+
+
+def remove_blanks(plist, atime, btime, stime):
+    """
+    :param plist: List to ammend
+    :param atime: arrival time index
+    :param btime: burst time index
+    :param stime: service time index
+    :return: void
+    """
+    if plist[0][atime] > 0:
+        idle_time = plist[0][atime]
+        plist.insert(0, [
+            -1, 0, idle_time, 0, 0, idle_time
+        ])
+
+    for i in range(len(plist)):
+        if i == len(plist) - 1:
+            break
+        idle_time = plist[i + 1][stime] - (plist[i][btime] + plist[i][stime])
+        if idle_time > 0:
+            # Insert Idle Period
+            plist.insert(i + 1, [
+                -1, (plist[i][btime] + plist[i][stime]), idle_time,
+                (plist[i][btime] + plist[i][stime]), 0, idle_time
+            ])
 
 
 def fcfs(processes):
@@ -69,21 +97,21 @@ def fcfs(processes):
     processes.sort(key=lambda tup: tup[1])
 
     for process in processes:
-        proc_table.append(
-            [processes.index(process), process[1], process[2], time, (time - process[1]),
-             (time + process[2] - process[1])])
-
         if process[1] > time:
             time = process[1]
+
+        proc_table.append(
+            [process[0], process[1], process[2], time, (time - process[1]),
+             (time + process[2] - process[1])])
+
         gantt_chart[('P' + str(process[0]))] = time
         avg_waiting.append(time - process[1])
         avg_turnaround.append(time + process[2] - process[1])
         time += process[2]
 
     # Check for blanks (Idle time)
-    """
-    TODO
-    """
+    # List, indices of arrival time, burst time, service time
+    remove_blanks(proc_table, 1, 2, 3)
 
     print processes
     print proc_table
@@ -100,21 +128,12 @@ def sjf_non_preemptive(processes):
     :return: tuple(AVG Waiting time, AVG Turnaround Time)
     """
     global time
-    print processes
+    # print processes
     # Waiting Time = Service Time - Arrival Time
     # Sort input according to arrival time and burst time
     processes.sort(key=lambda tup: tup[2])
     processes.sort(key=lambda tup: tup[1])
-    print processes
-
-    # Add to proc_table
-    for process in processes:
-        proc_table.append([
-            process[0], process[1], process[2]
-        ])
-        final_ans.append([
-            process[0], process[1], process[2]
-        ])
+    # print processes
 
     # Calculate total time of execution
     # Total Time = SUM(Burst Time) + Arrival Time of First Process
@@ -129,10 +148,12 @@ def sjf_non_preemptive(processes):
 
     # Execute first process
     gantt_chart[('P' + str(processes[0][0]))] = processes[0][1]
-    proc_table[0].append(processes[0][1])
+    proc_table.append([
+        processes[0][0], processes[0][1], processes[0][2], processes[0][1], 0, processes[0][2]
+    ])
     time += processes[0][2]
     # Remove from queue
-    print processes[0]
+    # print processes[0]
     processes.remove(processes[0])
     while time < total_time:
         # Group all processes where arrival time < current time
@@ -143,11 +164,14 @@ def sjf_non_preemptive(processes):
         # Sort ready queue by shortest time
         ready_queue.sort(key=lambda tup: tup[2])
         gantt_chart['P' + str(ready_queue[0][0])] = time
-        proc_table[proc_table.index(list(ready_queue[0]))].append(time)
+        proc_table.append([
+            ready_queue[0][0], ready_queue[0][1], ready_queue[0][2], time,
+            (time - ready_queue[0][1]), (time + ready_queue[0][2] - ready_queue[0][1])
+        ])
         # Execute shortest job
         time += ready_queue[0][2]
         # Remove from queue
-        print ready_queue[0]
+        # print ready_queue[0]
         processes.remove(ready_queue[0])
         ready_queue = []
 
@@ -156,9 +180,81 @@ def sjf_non_preemptive(processes):
         avg_waiting.append(process[3] - process[1])
         avg_turnaround.append(process[3] + process[2] - process[1])
 
+    # Check for blanks (Idle time)
+    remove_blanks(proc_table, 1, 2, 3)
+
     print proc_table
-    print avg_waiting
-    print avg_turnaround
+    # print avg_waiting
+    # print avg_turnaround
+    print "AVG Waiting Time:    " + str(get_avg(avg_waiting))
+    print "AVG Turnaround Time: " + str(get_avg(avg_turnaround))
+    return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
+
+
+def sjf_preemptive(processes):
+    """
+    :param processes: list of tuples(ID, arrival time, burst time)
+    :return: tuple(AVG Waiting time, AVG Turnaround Time)
+    """
+    global time
+    # print processes
+    # Waiting Time = Service Time - Arrival Time
+    # Sort input according to arrival time and burst time
+    processes.sort(key=lambda tup: tup[2])
+    processes.sort(key=lambda tup: tup[1])
+    # print processes
+
+    # Calculate total time of execution
+    # Total Time = SUM(Burst Time) + Arrival Time of First Process
+    total_time = processes[0][1]
+    for process in processes:
+        total_time += process[2]
+
+    print "total time: " + str(total_time)
+    # Proceed to first process
+    time += processes[0][1]
+    ready_queue = []
+
+    # Execute first process
+    gantt_chart[('P' + str(processes[0][0]))] = processes[0][1]
+    proc_table.append([
+        processes[0][0], processes[0][1], processes[0][2], processes[0][1], 0, processes[0][2]
+    ])
+    time += processes[0][2]
+    # Remove from queue
+    # print processes[0]
+    processes.remove(processes[0])
+    while time < total_time:
+        # Group all processes where arrival time < current time
+        for process in processes:
+            if process[1] <= time:
+                ready_queue.append(process)
+
+        # Sort ready queue by shortest time
+        ready_queue.sort(key=lambda tup: tup[2])
+        gantt_chart['P' + str(ready_queue[0][0])] = time
+        proc_table.append([
+            ready_queue[0][0], ready_queue[0][1], ready_queue[0][2], time,
+            (time - ready_queue[0][1]), (time + ready_queue[0][2] - ready_queue[0][1])
+        ])
+        # Execute shortest job
+        time += ready_queue[0][2]
+        # Remove from queue
+        # print ready_queue[0]
+        processes.remove(ready_queue[0])
+        ready_queue = []
+
+    # Calculate Average and Turnaround Time
+    for process in proc_table:
+        avg_waiting.append(process[3] - process[1])
+        avg_turnaround.append(process[3] + process[2] - process[1])
+
+    # Check for blanks (Idle time)
+    remove_blanks(proc_table, 1, 2, 3)
+
+    print proc_table
+    # print avg_waiting
+    # print avg_turnaround
     print "AVG Waiting Time:    " + str(get_avg(avg_waiting))
     print "AVG Turnaround Time: " + str(get_avg(avg_turnaround))
     return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
@@ -170,18 +266,12 @@ def priority_non_preemptive(processes):
     :return: tuple(AVG Waiting time, AVG Turnaround Time)
     """
     global time
-    print processes
+    # print processes
     # Waiting Time = Service Time - Arrival Time
-    # Sort input according to arrival time and priority
+    # Sort input according to arrival time and burst time
     processes.sort(key=lambda tup: tup[3])
     processes.sort(key=lambda tup: tup[1])
-    print processes
-
-    # Add to proc_table
-    for process in processes:
-        proc_table.append([
-            process[0], process[1], process[2], process[3]
-        ])
+    # print processes
 
     # Calculate total time of execution
     # Total Time = SUM(Burst Time) + Arrival Time of First Process
@@ -196,10 +286,12 @@ def priority_non_preemptive(processes):
 
     # Execute first process
     gantt_chart[('P' + str(processes[0][0]))] = processes[0][1]
-    proc_table[0].append(processes[0][1])
+    proc_table.append([
+        processes[0][0], processes[0][1], processes[0][2], processes[0][3], processes[0][1], 0, processes[0][2]
+    ])
     time += processes[0][2]
     # Remove from queue
-    print processes[0]
+    # print processes[0]
     processes.remove(processes[0])
     while time < total_time:
         # Group all processes where arrival time < current time
@@ -207,14 +299,17 @@ def priority_non_preemptive(processes):
             if process[1] <= time:
                 ready_queue.append(process)
 
-        # Sort ready queue by priority
+        # Sort ready queue by shortest time
         ready_queue.sort(key=lambda tup: tup[3])
         gantt_chart['P' + str(ready_queue[0][0])] = time
-        proc_table[proc_table.index(list(ready_queue[0]))].append(time)
+        proc_table.append([
+            ready_queue[0][0], ready_queue[0][1], ready_queue[0][2], ready_queue[0][3], time,
+            (time - ready_queue[0][1]), (time + ready_queue[0][2] - ready_queue[0][1])
+        ])
         # Execute shortest job
         time += ready_queue[0][2]
         # Remove from queue
-        print ready_queue[0]
+        # print ready_queue[0]
         processes.remove(ready_queue[0])
         ready_queue = []
 
@@ -223,10 +318,12 @@ def priority_non_preemptive(processes):
         avg_waiting.append(process[4] - process[1])
         avg_turnaround.append(process[4] + process[2] - process[1])
 
-    print gantt_chart
+    # Check for blanks (Idle time)
+    remove_blanks(proc_table, 1, 2, 4)
+
     print proc_table
-    print avg_waiting
-    print avg_turnaround
+    # print avg_waiting
+    # print avg_turnaround
     print "AVG Waiting Time:    " + str(get_avg(avg_waiting))
     print "AVG Turnaround Time: " + str(get_avg(avg_turnaround))
     return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
