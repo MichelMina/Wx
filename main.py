986243f05962613ba4c4307bfa536f18edfaabec
michealmina2 <michealmina2@gmail.com>
2016-03-30 11:14:07 +0200

    ss

--- main.py
+++ main.py
@@ -1,141 +1,155 @@
 import wx
 import sys
 import schedulers
-def Wiki_Question_Handler(Answer):
-	if Answer == wx.ID_NO:
-		AA=wx.MessageDialog(None,"Please click help from the menu bar then choose Wiki\nTo understand how to use the program",'Wiki',wx.OK)
-		A=AA.ShowModal()
-		AA.Destroy()
 
+
+def Wiki_Question_Handler(Answer):
+    if Answer == wx.ID_NO:
+        AA = wx.MessageDialog(None,
+                              "Please click help from the menu bar then choose Wiki\nTo understand how to use the program",
+                              'Wiki', wx.OK)
+        A = AA.ShowModal()
+        AA.Destroy()
 
 
 class Sched(wx.Frame):
+    total_time = 0
+    average_waiting = 0
+    Process = []
+    Time_Slice = 0
+    count = 0
 
-	total_time = 0
-	average_waiting=0
-	Process=[]
-	Time_Slice=0
-	count = 0
+    def __init__(self, parent, id):
 
-	def __init__(self,parent,id):
+        # Did you read the wiki?
+        WikiQuestion = wx.MessageDialog(None, 'Did you read the "how to use" Wiki?', 'Title', wx.YES_NO)
+        WikiQuestion_Answer = WikiQuestion.ShowModal()
+        WikiQuestion.Destroy()
+        Wiki_Question_Handler(WikiQuestion_Answer)
 
+        # Single Choice Dialogue (Choose Scheduler mode)
+        select = wx.SingleChoiceDialog(None, 'Please choose the type of your desired scheduler', 'Scheduler Choice',
+                                       ['FCFS', 'SJF Preemptive',
+                                        'SJF non-Preemptive', 'Priority preepmtive', 'Priority non-Preemptive',
+                                        'Round Robin'])
+        if select.ShowModal() == wx.ID_CANCEL:
+            sys.exit()
+        Scheduler_Mode_Answer = select.GetStringSelection()
+        select.Destroy()
 
-		#Did you read the wiki?
-		WikiQuestion=wx.MessageDialog(None,'Did you read the "how to use" Wiki?','Title',wx.YES_NO)
-		WikiQuestion_Answer = WikiQuestion.ShowModal()
-		WikiQuestion.Destroy()
-		Wiki_Question_Handler(WikiQuestion_Answer)
-
-		#Single Choice Dialogue (Choose Scheduler mode)
-		select=wx.SingleChoiceDialog(None,'Please choose the type of your desired scheduler','Scheduler Choice',['FCFS','SJF Preemptive',
-					'SJF non-Preemptive','Priority preepmtive','Priority non-Preemptive','Round Robin'])
-		if select.ShowModal()== wx.ID_CANCEL:
-			sys.exit()
-		Scheduler_Mode_Answer=select.GetStringSelection()
-		select.Destroy()
-
-		#Main Frame
-		image = 'roses.jpg'
-		bmp1 = wx.Image(image, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-		wx.Frame.__init__(self,parent,id,Scheduler_Mode_Answer+" Scheduler",size=(bmp1.GetWidth(),bmp1.GetHeight()))
-		panel=wx.Panel(self)
-		panel.SetBackgroundColour('green')
-		self.bitmap1 = wx.StaticBitmap(self, -1, bmp1, (0, 0))
-		panel = self.bitmap1
+        # Main Frame
+        image = 'roses.jpg'
+        self.bmp1 = wx.Image(image, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.Width = self.bmp1.GetWidth()
+        self.Hight = self.bmp1.GetHeight()
+        wx.Frame.__init__(self, parent, id, Scheduler_Mode_Answer + " Scheduler", size=(self.Width, self.Hight))
+        panel = wx.Panel(self)
+        panel.SetBackgroundColour('green')
+        self.bitmap1 = wx.StaticBitmap(self, -1, self.bmp1, (0, 0))
+        panel = self.bitmap1
 
 
-		#Arrival Time things
-		Arrival_Time_Static=wx.StaticText(panel,-1,'Arrival time',pos=(10,10),size=(-1,-1),style=0)
-		Arrival_Time_Static.SetForegroundColour('white')
-		Arrival_Time_Text=wx.TextCtrl(panel,pos=(75,8),size=(100,20))
-		Arrival_Time_Text.Value='0'
-		Arrival_Time_Text.SetBackgroundColour('pink')
 
-		#Burst Time things
-		Burst_Time_Static=wx.StaticText(panel,-1,'Burst time',pos=(10,30),size=(-1,-1),style=0)
-		Burst_Time_Static.SetForegroundColour('white')
-		Burst_Time_Text=wx.TextCtrl(panel,pos=(75,28),size=(100,20))
-		Burst_Time_Text.Value='0'
-		Burst_Time_Text.SetBackgroundColour('pink')
+        # Arrival Time things
+        Arrival_Time_Static = wx.StaticText(panel, -1, 'Arrival time', pos=(10, 10), size=(-1, -1), style=0)
+        Arrival_Time_Static.SetForegroundColour('white')
+        Arrival_Time_Text = wx.TextCtrl(panel, pos=(75, 8), size=(100, 20))
+        Arrival_Time_Text.Value = '0'
+        Arrival_Time_Text.SetBackgroundColour('TAN')
 
-		#Case that takes priority
-		if Scheduler_Mode_Answer == 'Priority preepmtive' or Scheduler_Mode_Answer == 'Priority non-Preemptive':
-			Priority_Static= wx.StaticText(panel,-1,'Priority',pos=(10,50),size=(-1,-1),style=0)
-			Priority_Text= wx.TextCtrl(panel,pos=(75,48),size=(100,20))
-			Priority_Text.Value='100'
+        # Burst Time things
+        Burst_Time_Static = wx.StaticText(panel, -1, 'Burst time', pos=(10, 30), size=(-1, -1), style=0)
+        Burst_Time_Static.SetForegroundColour('white')
+        Burst_Time_Text = wx.TextCtrl(panel, pos=(75, 28), size=(100, 20))
+        Burst_Time_Text.Value = '0'
+        Burst_Time_Text.SetBackgroundColour('THISTLE')
 
-		#Case Round robin
-		if Scheduler_Mode_Answer == 'Round Robin':
-			Slice_Static= wx.StaticText(panel,-1,'Time Slice',pos=(10,50),size=(-1,-1),style=0)
-			Time_Slice_Spinner=wx.SpinCtrl(panel, -1, "", (75,48), (100,20))
-			Time_Slice_Spinner.Value='50'
+        # Case that takes priority
+        if Scheduler_Mode_Answer == 'Priority preepmtive' or Scheduler_Mode_Answer == 'Priority non-Preemptive':
+            Priority_Static = wx.StaticText(panel, -1, 'Priority', pos=(10, 50), size=(-1, -1), style=0)
+            Priority_Text = wx.TextCtrl(panel, pos=(75, 48), size=(100, 20))
+            Priority_Text.Value = '100'
 
-		#Add process button
-		Add_Process=wx.Button(panel,label="Add process",pos=(200,15),size=(-1,-1))
-		Add_Process.SetBackgroundColour('pink')
+        # Case Round robin
+        if Scheduler_Mode_Answer == 'Round Robin':
+            Slice_Static = wx.StaticText(panel, -1, 'Time Slice', pos=(10, 50), size=(-1, -1), style=0)
+            Time_Slice_Spinner = wx.SpinCtrl(panel, -1, "", (75, 48), (100, 20))
+            Time_Slice_Spinner.SetValue(50)
 
-		#Add button action event handlers
-		if Scheduler_Mode_Answer == 'FCFS' or Scheduler_Mode_Answer == 'SJF preepmtive' or Scheduler_Mode_Answer == 'SJF non-Preemptive':
-			self.Bind(wx.EVT_BUTTON,lambda event: self.Add_Process_EVT(event,Burst_Time_Text,Arrival_Time_Text), Add_Process)
+        # Add process button
+        Add_Process = wx.Button(panel, label="Add process", pos=(200, 15), size=(-1, -1))
+        Add_Process.SetBackgroundColour('MAROON')
 
-		elif Scheduler_Mode_Answer == 'Priority preepmtive' or Scheduler_Mode_Answer == 'Priority non-Preemptive':
-			self.Bind(wx.EVT_BUTTON,lambda event: self.Add_Process_EVT_P(event,Burst_Time_Text,Arrival_Time_Text,Priority_Text), Add_Process)
+        # Add button action event handlers
+        if Scheduler_Mode_Answer == 'FCFS' or Scheduler_Mode_Answer == 'SJF preepmtive' or Scheduler_Mode_Answer == 'SJF non-Preemptive':
+            self.Bind(wx.EVT_BUTTON, lambda event: self.Add_Process_EVT(event, Burst_Time_Text, Arrival_Time_Text),
+                      Add_Process)
 
-		elif Scheduler_Mode_Answer == 'Round Robin':
-			self.Bind(wx.EVT_BUTTON,lambda event: self.Add_Process_EVT_T(event,Burst_Time_Text,Arrival_Time_Text,Time_Slice_Spinner,panel), Add_Process)
+        elif Scheduler_Mode_Answer == 'Priority preepmtive' or Scheduler_Mode_Answer == 'Priority non-Preemptive':
+            self.Bind(wx.EVT_BUTTON,
+                      lambda event: self.Add_Process_EVT_P(event, Burst_Time_Text, Arrival_Time_Text, Priority_Text),
+                      Add_Process)
 
-		#Finish process addition button
-		Finish=wx.Button(panel,label="Done",pos=(300,15),size=(-1,-1))
-		self.Bind(wx.EVT_BUTTON,lambda event: self.Finish_EVT(event,Scheduler_Mode_Answer), Finish)
-		Finish.SetBackgroundColour('pink')
+        elif Scheduler_Mode_Answer == 'Round Robin':
+            self.Bind(wx.EVT_BUTTON, lambda event: self.Add_Process_EVT_T(event, Burst_Time_Text, Arrival_Time_Text,
+                                                                          Time_Slice_Spinner, panel), Add_Process)
 
-	#Normal Schedulers
-	def Add_Process_EVT(self,event,Burst,Arrival):
-		self.Process.append((self.count,float(Arrival.GetValue()),float(Burst.GetValue())))
-		Burst.Value='0'
-		Arrival.Value='0'
-		self.count+=1
+        # Finish process addition button
+        Finish = wx.Button(panel, label="Done", pos=(300, 15), size=(-1, -1))
+        self.Bind(wx.EVT_BUTTON, lambda event: self.Finish_EVT(event, Scheduler_Mode_Answer), Finish)
+        Finish.SetBackgroundColour('PLUM')
 
-	#Priority Schedulers
-	def Add_Process_EVT_P(self,event,Burst,Arrival,Priority):
-		self.Process.append((self.count,float(Arrival.GetValue()),float(Burst.GetValue()),int(Priority.GetValue())))
-		self.count +=1
-		Burst.Value='0'
-		Arrival.Value='0'
-		Priority.Value='100'
+    # Normal Schedulers
+    def Add_Process_EVT(self, event, Burst, Arrival):
+        self.Process.append((self.count, float(Arrival.GetValue()), float(Burst.GetValue())))
+        Burst.Value = '0'
+        Arrival.Value = '0'
+        self.count += 1
 
-	#Round Robin Schedulers
-	def Add_Process_EVT_T(self,event,Burst,Arrival,Time_Slice_Spinner,Panel):
-		self.Process.append((self.count,float(Arrival.GetValue()),float(Burst.GetValue())))
-		self.count +=1
-		Burst.Value='0'
-		Arrival.Value='0'
-		wx.StaticText()
-		if Time_Slice_Spinner:
-			self.Time_Slice=Time_Slice_Spinner.GetValue()
-		Priority_Static= wx.StaticText(Panel,-1,' = '+(str)(self.Time_Slice),pos=(75,50),size=(-1,-1),style=0)
-		Time_Slice_Spinner.Hide()
+    # Priority Schedulers
+    def Add_Process_EVT_P(self, event, Burst, Arrival, Priority):
+        self.Process.append((self.count, float(Arrival.GetValue()), float(Burst.GetValue()), int(Priority.GetValue())))
+        self.count += 1
+        Burst.Value = '0'
+        Arrival.Value = '0'
+        Priority.Value = '100'
 
-	def Finish_EVT(self,event,Scheduler_Type):
-		print (self.Process)
-		if Scheduler_Type == 'FCFS':
-			schedulers.fcfs(self.Process)
-		elif Scheduler_Type == 'SJF Preemptive':
-			schedulers.sjf_preemptive(self.Process)
-		elif Scheduler_Type == 'SJF non-Preemptive':
-			schedulers.sjf_non_preemptive(self.Process)
-		elif Scheduler_Type == 'Priority Preemptive':
-			schedulers.priority_preemptive(self.Process)
-		elif Scheduler_Type == 'Priority non-Preemptive':
-			schedulers.priority_non_preemptive(self.Process)
-		elif Scheduler_Type == 'Round Robin':
-			schedulers.round_robin_non_preemptive(self.Process, self.Time_Slice)
+    # Round Robin Schedulers
+    def Add_Process_EVT_T(self, event, Burst, Arrival, Time_Slice_Spinner, Panel):
+        self.Process.append((self.count, float(Arrival.GetValue()), float(Burst.GetValue())))
+        self.count += 1
+        Burst.Value = '0'
+        Arrival.Value = '0'
+        # wx.StaticText()
+        if Time_Slice_Spinner:
+            self.Time_Slice = Time_Slice_Spinner.GetValue()
+        Priority_Static = wx.StaticText(Panel, -1, ' = ' + (str)(self.Time_Slice), pos=(75, 50), size=(-1, -1), style=0)
+        Time_Slice_Spinner.Hide()
 
-		sys.exit()
+    def Finish_EVT(self, event, Scheduler_Type):
+        print (self.Process)
+        if Scheduler_Type == 'FCFS':
+            schedulers.fcfs(self.Process)
+        elif Scheduler_Type == 'SJF Preemptive':
+            schedulers.sjf_preemptive(self.Process)
+        elif Scheduler_Type == 'SJF non-Preemptive':
+            schedulers.sjf_non_preemptive(self.Process)
+        elif Scheduler_Type == 'Priority Preemptive':
+            schedulers.priority_preemptive(self.Process)
+        elif Scheduler_Type == 'Priority non-Preemptive':
+            schedulers.priority_non_preemptive(self.Process)
+        elif Scheduler_Type == 'Round Robin':
+            im = schedulers.round_robin_non_preemptive(self.Process, self.Time_Slice)
 
-if __name__=='__main__' :
-	app=wx.PySimpleApp(0)
-	frame=Sched(parent=None,id=-1)
-	frame.Show(True)
-	app.MainLoop()
-	
+        image1 = "out.png"
+        bmp1 = wx.Image(image1, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.bitmap21 = wx.StaticBitmap(self, -1, bmp1, (0, 0))
+
+        # sys.exit()
+
+
+if __name__ == '__main__':
+    app = wx.App(0)
+    frame = Sched(parent=None, id=-1)
+    frame.Show(True)
+    app.MainLoop()
--- schedulers.py
+++ schedulers.py
@@ -1,7 +1,9 @@
 import datetime
-import gantt
+# import gantt
 from time import localtime, mktime
 from collections import OrderedDict
+import Queue as queue
+from googlegantt import GanttChart, GanttCategory
 
 
 def draw_svg(info):
@@ -38,46 +40,18 @@ def get_avg(values):
 proc_table = []
 avg_waiting = []
 avg_turnaround = []
+final_ans = []
 gantt_chart = OrderedDict()
 time = 0
 
 
 def reset():
-    """
-    :return: Void
-    """
     global proc_table, avg_waiting, avg_turnaround, time
     proc_table = []
     avg_waiting = []
     avg_turnaround = []
 
     time = 0
-
-
-def remove_blanks(plist, atime, btime, stime):
-    """
-    :param plist: List to ammend
-    :param atime: arrival time index
-    :param btime: burst time index
-    :param stime: service time index
-    :return: void
-    """
-    if plist[0][atime] > 0:
-        idle_time = plist[0][atime]
-        plist.insert(0, [
-            -1, 0, idle_time, 0, 0, idle_time
-        ])
-
-    for i in range(len(plist)):
-        if i == len(plist) - 1:
-            break
-        idle_time = plist[i + 1][stime] - (plist[i][btime] + plist[i][stime])
-        if idle_time > 0:
-            # Insert Idle Period
-            plist.insert(i + 1, [
-                -1, (plist[i][btime] + plist[i][stime]), idle_time,
-                (plist[i][btime] + plist[i][stime]), 0, idle_time
-            ])
 
 
 def fcfs(processes):
@@ -97,27 +71,27 @@ def fcfs(processes):
     processes.sort(key=lambda tup: tup[1])
 
     for process in processes:
-        if process[1] > time:
-            time = process[1]
-
         proc_table.append(
-            [process[0], process[1], process[2], time, (time - process[1]),
+            [processes.index(process), process[1], process[2], time, (time - process[1]),
              (time + process[2] - process[1])])
 
+        if process[1] > time:
+            time = process[1]
         gantt_chart[('P' + str(process[0]))] = time
         avg_waiting.append(time - process[1])
         avg_turnaround.append(time + process[2] - process[1])
         time += process[2]
 
     # Check for blanks (Idle time)
-    # List, indices of arrival time, burst time, service time
-    remove_blanks(proc_table, 1, 2, 3)
+    """
+    TODO
+    """
 
-    print processes
-    print proc_table
-    print gantt_chart
-    print "Average Waiting Time:    " + str(get_avg(avg_waiting))
-    print "Average Turnaround Time: " + str(get_avg(avg_turnaround))
+    print (processes)
+    print (proc_table)
+    print (gantt_chart)
+    print ("Average Waiting Time:    " + str(get_avg(avg_waiting)))
+    print ("Average Turnaround Time: " + str(get_avg(avg_turnaround)))
 
     return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
 
@@ -128,12 +102,21 @@ def sjf_non_preemptive(processes):
     :return: tuple(AVG Waiting time, AVG Turnaround Time)
     """
     global time
-    # print processes
+    print (processes)
     # Waiting Time = Service Time - Arrival Time
     # Sort input according to arrival time and burst time
     processes.sort(key=lambda tup: tup[2])
     processes.sort(key=lambda tup: tup[1])
-    # print processes
+    print (processes)
+
+    # Add to proc_table
+    for process in processes:
+        proc_table.append([
+            process[0], process[1], process[2]
+        ])
+        final_ans.append([
+            process[0], process[1], process[2]
+        ])
 
     # Calculate total time of execution
     # Total Time = SUM(Burst Time) + Arrival Time of First Process
@@ -141,19 +124,17 @@ def sjf_non_preemptive(processes):
     for process in processes:
         total_time += process[2]
 
-    print "total time: " + str(total_time)
+    print ("total time: " + str(total_time))
     # Proceed to first process
     time += processes[0][1]
     ready_queue = []
 
     # Execute first process
     gantt_chart[('P' + str(processes[0][0]))] = processes[0][1]
-    proc_table.append([
-        processes[0][0], processes[0][1], processes[0][2], processes[0][1], 0, processes[0][2]
-    ])
+    proc_table[0].append(processes[0][1])
     time += processes[0][2]
     # Remove from queue
-    # print processes[0]
+    print (processes[0])
     processes.remove(processes[0])
     while time < total_time:
         # Group all processes where arrival time < current time
@@ -164,14 +145,11 @@ def sjf_non_preemptive(processes):
         # Sort ready queue by shortest time
         ready_queue.sort(key=lambda tup: tup[2])
         gantt_chart['P' + str(ready_queue[0][0])] = time
-        proc_table.append([
-            ready_queue[0][0], ready_queue[0][1], ready_queue[0][2], time,
-            (time - ready_queue[0][1]), (time + ready_queue[0][2] - ready_queue[0][1])
-        ])
+        proc_table[proc_table.index(list(ready_queue[0]))].append(time)
         # Execute shortest job
         time += ready_queue[0][2]
         # Remove from queue
-        # print ready_queue[0]
+        print (ready_queue[0])
         processes.remove(ready_queue[0])
         ready_queue = []
 
@@ -180,83 +158,11 @@ def sjf_non_preemptive(processes):
         avg_waiting.append(process[3] - process[1])
         avg_turnaround.append(process[3] + process[2] - process[1])
 
-    # Check for blanks (Idle time)
-    remove_blanks(proc_table, 1, 2, 3)
-
-    print proc_table
-    # print avg_waiting
-    # print avg_turnaround
-    print "AVG Waiting Time:    " + str(get_avg(avg_waiting))
-    print "AVG Turnaround Time: " + str(get_avg(avg_turnaround))
-    return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
-
-
-def sjf_preemptive(processes):
-    """
-    :param processes: list of tuples(ID, arrival time, burst time)
-    :return: tuple(AVG Waiting time, AVG Turnaround Time)
-    """
-    global time
-    # print processes
-    # Waiting Time = Service Time - Arrival Time
-    # Sort input according to arrival time and burst time
-    processes.sort(key=lambda tup: tup[2])
-    processes.sort(key=lambda tup: tup[1])
-    # print processes
-
-    # Calculate total time of execution
-    # Total Time = SUM(Burst Time) + Arrival Time of First Process
-    total_time = processes[0][1]
-    for process in processes:
-        total_time += process[2]
-
-    print "total time: " + str(total_time)
-    # Proceed to first process
-    time += processes[0][1]
-    ready_queue = []
-
-    # Execute first process
-    gantt_chart[('P' + str(processes[0][0]))] = processes[0][1]
-    proc_table.append([
-        processes[0][0], processes[0][1], processes[0][2], processes[0][1], 0, processes[0][2]
-    ])
-    time += processes[0][2]
-    # Remove from queue
-    # print processes[0]
-    processes.remove(processes[0])
-    while time < total_time:
-        # Group all processes where arrival time < current time
-        for process in processes:
-            if process[1] <= time:
-                ready_queue.append(process)
-
-        # Sort ready queue by shortest time
-        ready_queue.sort(key=lambda tup: tup[2])
-        gantt_chart['P' + str(ready_queue[0][0])] = time
-        proc_table.append([
-            ready_queue[0][0], ready_queue[0][1], ready_queue[0][2], time,
-            (time - ready_queue[0][1]), (time + ready_queue[0][2] - ready_queue[0][1])
-        ])
-        # Execute shortest job
-        time += ready_queue[0][2]
-        # Remove from queue
-        # print ready_queue[0]
-        processes.remove(ready_queue[0])
-        ready_queue = []
-
-    # Calculate Average and Turnaround Time
-    for process in proc_table:
-        avg_waiting.append(process[3] - process[1])
-        avg_turnaround.append(process[3] + process[2] - process[1])
-
-    # Check for blanks (Idle time)
-    remove_blanks(proc_table, 1, 2, 3)
-
-    print proc_table
-    # print avg_waiting
-    # print avg_turnaround
-    print "AVG Waiting Time:    " + str(get_avg(avg_waiting))
-    print "AVG Turnaround Time: " + str(get_avg(avg_turnaround))
+    print (proc_table)
+    print (avg_waiting)
+    print (avg_turnaround)
+    print ("AVG Waiting Time:    " + str(get_avg(avg_waiting)))
+    print ("AVG Turnaround Time: " + str(get_avg(avg_turnaround)))
     return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
 
 
@@ -266,12 +172,18 @@ def priority_non_preemptive(processes):
     :return: tuple(AVG Waiting time, AVG Turnaround Time)
     """
     global time
-    # print processes
+    print (processes)
     # Waiting Time = Service Time - Arrival Time
-    # Sort input according to arrival time and burst time
+    # Sort input according to arrival time and priority
     processes.sort(key=lambda tup: tup[3])
     processes.sort(key=lambda tup: tup[1])
-    # print processes
+    print (processes)
+
+    # Add to proc_table
+    for process in processes:
+        proc_table.append([
+            process[0], process[1], process[2], process[3]
+        ])
 
     # Calculate total time of execution
     # Total Time = SUM(Burst Time) + Arrival Time of First Process
@@ -279,19 +191,17 @@ def priority_non_preemptive(processes):
     for process in processes:
         total_time += process[2]
 
-    print "total time: " + str(total_time)
+    print ("total time: " + str(total_time))
     # Proceed to first process
     time += processes[0][1]
     ready_queue = []
 
     # Execute first process
     gantt_chart[('P' + str(processes[0][0]))] = processes[0][1]
-    proc_table.append([
-        processes[0][0], processes[0][1], processes[0][2], processes[0][3], processes[0][1], 0, processes[0][2]
-    ])
+    proc_table[0].append(processes[0][1])
     time += processes[0][2]
     # Remove from queue
-    # print processes[0]
+    print (processes[0])
     processes.remove(processes[0])
     while time < total_time:
         # Group all processes where arrival time < current time
@@ -299,17 +209,14 @@ def priority_non_preemptive(processes):
             if process[1] <= time:
                 ready_queue.append(process)
 
-        # Sort ready queue by shortest time
+        # Sort ready queue by priority
         ready_queue.sort(key=lambda tup: tup[3])
         gantt_chart['P' + str(ready_queue[0][0])] = time
-        proc_table.append([
-            ready_queue[0][0], ready_queue[0][1], ready_queue[0][2], ready_queue[0][3], time,
-            (time - ready_queue[0][1]), (time + ready_queue[0][2] - ready_queue[0][1])
-        ])
+        proc_table[proc_table.index(list(ready_queue[0]))].append(time)
         # Execute shortest job
         time += ready_queue[0][2]
         # Remove from queue
-        # print ready_queue[0]
+        print (ready_queue[0])
         processes.remove(ready_queue[0])
         ready_queue = []
 
@@ -318,14 +225,12 @@ def priority_non_preemptive(processes):
         avg_waiting.append(process[4] - process[1])
         avg_turnaround.append(process[4] + process[2] - process[1])
 
-    # Check for blanks (Idle time)
-    remove_blanks(proc_table, 1, 2, 4)
-
-    print proc_table
-    # print avg_waiting
-    # print avg_turnaround
-    print "AVG Waiting Time:    " + str(get_avg(avg_waiting))
-    print "AVG Turnaround Time: " + str(get_avg(avg_turnaround))
+    print (gantt_chart)
+    print (proc_table)
+    print (avg_waiting)
+    print (avg_turnaround)
+    print ("AVG Waiting Time:    " + str(get_avg(avg_waiting)))
+    print ("AVG Turnaround Time: " + str(get_avg(avg_turnaround)))
     return tuple((get_avg(avg_waiting), get_avg(avg_turnaround)))
 
 
@@ -336,6 +241,71 @@ def round_robin_non_preemptive(processes, time_slice):
     :return: tuple(AVG Waiting time, AVG Turnaround Time)
     """
     global time
+    # constants to ease the readability, like #define represents location of each in the tuple of process
+    ID = 0
+    Arrival = 1
+    Burst = 2
+    Av_wait = 3
+    Last_Arr = 4
+    # Index to be used later
+    Standing_index = 0
+    Total_waiting = 0
 
     # Sort input according to arrival time
     processes.sort(key=lambda tup: tup[1])
+
+    Ex_Queue = queue.Queue(len(processes))
+    count = -1
+    for process in processes:
+        count += 1
+        # each process in the table have ID,Arrival time,Burst, its waiting time, its last arrival time
+        proc_table.append([process[ID], process[Arrival], process[Burst], 0, process[Arrival]])
+        # put whichever arrived at time 0
+        if process[Arrival] <= time:
+            Ex_Queue.put(proc_table[count])
+            Standing_index += 1  # increment standing index (next to be checked if arrived)
+
+    gc = GanttChart('Test Chart', width=800, height=200, progress=(2011, 02, 27))
+    on_time = GanttCategory('On Time', '0c0')
+    late = GanttCategory('Late', 'c00')
+
+    t1 = gc.add_task('Late Task', (2016, 1, 1), duration=0, category=late)
+
+    while Standing_index < len(processes) or Ex_Queue._qsize():
+
+        # if the arrival time of process <= time .. put it into ready
+        if Standing_index <= len(processes) - 1:
+            dummy = proc_table[Standing_index]
+            if dummy[Arrival] <= time:
+                Ex_Queue._put(dummy)
+                Standing_index += 1
+
+        if Ex_Queue._qsize() > 0:
+
+            dummy = Ex_Queue._get()
+            if dummy[Burst] > time_slice:
+                # executing
+                dummy[Burst] = dummy[Burst] - time_slice
+                # Increment its total waiting time
+                dummy[Av_wait] = dummy[Av_wait] + (time - dummy[Last_Arr])
+                # setting its time arrival
+                dummy[Last_Arr] = time + time_slice
+                # pushing it to the queue
+                Ex_Queue.put(dummy)
+                t1 = gc.add_task('P' + str(dummy[ID]), depends_on=t1, duration=time_slice, category=on_time)
+
+                time += time_slice
+                print("executing P%d at time %d" % (dummy[ID], time - time_slice))
+            elif 0 < dummy[Burst] <= time_slice:
+                # Update total waiting time
+                Total_waiting += (dummy[3] + (time - (dummy[4])))
+                # finish the job incrementing the time
+                time += dummy[Burst]
+                print("ending P%d at time %d" % (dummy[ID], time - dummy[Burst]))
+                t1 = gc.add_task('P' + str(dummy[ID]), depends_on=t1, duration=dummy[Burst], category=on_time)
+        else:
+            print("bubble at time = %d" % time)
+            time += 1
+    image = gc.get_image('out.png')
+    print ("Average waiting time = %f" % (Total_waiting / len(processes)))
+    print image
